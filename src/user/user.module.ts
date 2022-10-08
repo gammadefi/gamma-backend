c271cbdfb104610759d48431b5a76c7ff5e6d082
smith2eric <ericsmithsonian2@gmail.com>
2022-10-08 23:14:38 +0100

    [update]: added UserControllers to controllers array and included imports arrays

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
+  imports: [],
   providers: [UserService, PrismaService],
+  controllers: [UserController],
 })
 export class UserModule {}
