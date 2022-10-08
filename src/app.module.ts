48df464650d2abddfad3767dd6a6524a15b4c1b6
smith2eric <ericsmithsonian2@gmail.com>
2022-10-08 23:12:37 +0100

    [update]: added UserService to list of providers

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 import { PrismaService } from './prisma/prisma.service';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [UserModule],
   controllers: [AppController, UserController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, UserService, PrismaService],
 })
 export class AppModule {}
